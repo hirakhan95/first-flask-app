0c5f2823edf7dcade45d919f004b8de96d7f5701
Hira Khan <phr.hirakhan@gmail.com>
2022-05-17 12:04:06 +0200

    made changes

--- main.py
+++ main.py
@@ -85,4 +85,10 @@ def logout():
 
 if __name__ == '__main__':
     db.create_all()
-    app.run(debug=True)
+
+    isProduction = True
+
+    if not isProduction:
+        app.run(debug=True)
+    else:
+        app.run(debug=False, host='0.0.0.0', port='3312')
